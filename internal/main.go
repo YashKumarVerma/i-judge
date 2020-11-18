938f112b19bf40fce1f5d3a928b84f55c284f4a6
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-11-18 13:26:24 +0530

    feat: working code with io pipes

--- internal/main.go
+++ internal/main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
 	ui "github.com/YashKumarVerma/go-lib-ui"
-	"github.com/YashKumarVerma/i-judge/internal/config"
 )
 
 func main() {
-	config.Initialize()
-	ui.ContextPrint("balance_scale", config.Load.Name)
+	inputString := "5\napple\n"
+	ui.ContextPrint("balance_scale", "I Judge")
+	ui.ContextPrint("moai", "Input :")
+	fmt.Println(inputString)
+
+	// some mojo here
+	rescueStdout := os.Stdout
+	read, write, _ := os.Pipe()
+	os.Stdout = write
+
+	//Just for testing, replace with your subProcess
+	subProcess := exec.Command("/home/yash/Desktop/files/works/projects/i-judge/codes/question1/a.out")
+	stdin, err := subProcess.StdinPipe()
+	ui.CheckError(err, "Error creating stdin pipe", true)
+	defer stdin.Close()
+
+	subProcess.Stdout = os.Stdout
+	subProcess.Stderr = os.Stderr
+
+	err = subProcess.Start()
+	ui.CheckError(err, "Error executing code", true)
+
+	io.WriteString(stdin, inputString)
+
+	// close pipes
+	write.Close()
+	out, _ := ioutil.ReadAll(read)
+	os.Stdout = rescueStdout
+
+	// final display
+	ui.ContextPrint("moai", "Output")
+	fmt.Println(string(out))
 }
